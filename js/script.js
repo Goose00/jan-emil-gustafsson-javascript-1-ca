0f7d389a9da77ab20346db53aacff9e08fcdebf5
Goose00 <jan87@live.no>
2020-05-25 16:05:24 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-fetch("https://superhero-search.p.rapidapi.com/?hero=spiderman", {
+fetch("https://imdb-internet-movie-database-unofficial.p.rapidapi.com/film/tt0770828", {
 	"method": "GET",
 	"headers": {
-		"x-rapidapi-host": "superhero-search.p.rapidapi.com",
+		"x-rapidapi-host": "imdb-internet-movie-database-unofficial.p.rapidapi.com",
 		"x-rapidapi-key": "3cbd8fe6e8msh154d62eb58c16c4p1fe8d5jsnc9c87e469b9c"
 	}
 })
@@ -11,4 +11,3 @@ fetch("https://superhero-search.p.rapidapi.com/?hero=spiderman", {
 .catch(err => {
 	console.log(err);
 });
-
